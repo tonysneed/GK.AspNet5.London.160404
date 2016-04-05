8c166115ed8a019451c770f6bf644a96645724ba
Anthony Sneed <tony@tonysneed.com>
2016-04-05 13:10:33 +0100

    Added HelloMvc project.

---
+++ Demos/HelloMvc/src/HelloMvc/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using HelloMvc.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMvc.Controllers
+{
+    //[Route(""), Route("Home")]
+    [Route("[controller]"), Route("")]
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        //[Route(""), Route("Index")]
+        [Route("[action]"), Route("")]
+        public IActionResult Index()
+        {
+            //ViewData["name"] = "Tony";
+            //ViewBag.lastname = "Sneed";
+            var vm = new CustomerViewModel
+            {
+                FirstName = "John",
+                LastName = "Lennon"
+            };
+            return View(vm);
+        }
+
+        // POST
+        [HttpPost]
+        [Route("Edit")]
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        // GET: /<controller>/5
+        [Route("{id:int}")]
+        public IActionResult Index(int id)
+        {
+            return Content($"Index with id {id}");
+        }
+
+        // GET: hello?city=London
+        [Route("Hello/{city?}")]
+        public IActionResult SayHello(string city = "London")
+        {
+            return Content($"Hello {city}");
+        }
+
+        [Route("Goodbye/{city?}")]
+        public IActionResult SayGoodbye(string city = "London")
+        {
+            return Content($"Goodbye {city}");
+        }
+    }
+}
---
+++ Demos/HelloMvc/src/HelloMvc/Models/CustomerViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloMvc.Models
+{
+    public class CustomerViewModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
