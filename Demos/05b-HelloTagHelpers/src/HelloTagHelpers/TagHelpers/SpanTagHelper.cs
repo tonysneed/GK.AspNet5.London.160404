1dcd446bfbb06711462373404fa86c92814b7d94
Anthony Sneed <tony@tonysneed.com>
2016-04-06 13:50:45 +0100

    Added Tag Helpers demo.

---
+++ Demos/05b-HelloTagHelpers/src/HelloTagHelpers/TagHelpers/SpanTagHelper.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Razor.TagHelpers;
+
+namespace HelloTagHelpers.TagHelpers
+{
+    public class SpanTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context,
+            TagHelperOutput output)
+        {
+            var emoji = context.AllAttributes["emoji"];
+            if (emoji != null && emoji.Value.ToString() == "smile")
+            {
+                output.Content.SetContent(" :) ");
+                output.TagMode = TagMode.StartTagAndEndTag;
+            }
+        }
+    }
+}
